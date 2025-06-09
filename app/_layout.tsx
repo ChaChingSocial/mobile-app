7b19308b416261d9d336735cca2710b3d12fd45c
sonialomo <sonia.lomo@fatfiresocial.com>
2025-06-09 09:01:37 +0300

    feat: google sign in button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,27 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 import { SessionProvider } from "@/lib/providers/AuthContext";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 
-
 export default function RootLayout() {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+      // scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
+      // offlineAccess: true,
+      // hostedDomain: "", // specifies a hosted domain restriction
+      // forceCodeForRefreshToken: false, // [Android] related to `serverAuthCode`, read the docs link below *.
+      // accountName: "", // [Android] specifies an account name on the device that should be used
+      iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
+      // openIdRealm: "", // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+      profileImageSize: 150, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+    });
+  }, []);
+
   return (
     <SessionProvider>
       <GluestackUIProvider mode="light">
--- app/login.tsx
+++ app/login.tsx
@@ -6,12 +6,19 @@ import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { loginWithEmail } from "@/lib/api/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useStorageState } from "@/hooks/useStorageState";
 import { auth } from "@/config/firebase";
 import { userApi } from "@/config/backend";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  isErrorWithCode,
+  isSuccessResponse,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 
 export default function LoginScreen() {
   const [_, setSession] = useStorageState("session");
@@ -23,6 +30,8 @@ export default function LoginScreen() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [showPassword, setShowPassword] = useState(false);
+  const [googleResponse, setGoogleResponse] = useState<any>(null);
+  const [googleLoading, setGoogleLoading] = useState(false);
 
   const handleLogin = async () => {
     try {
@@ -37,7 +46,6 @@ export default function LoginScreen() {
       // setEmail("");
       // setPassword("");
       signIn();
-
     } catch (error) {
       console.log("error", error);
       setError(
@@ -52,6 +60,32 @@ export default function LoginScreen() {
     });
   };
 
+  const GoogleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        setGoogleResponse(response.data);
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
 
   return (
     <FormControl className="bg-white p-4 border rounded-lg border-outline-300 mx-6">
@@ -76,9 +110,22 @@ export default function LoginScreen() {
           </Input>
         </VStack>
         <Button className="ml-auto" onPress={handleLogin}>
-          <ButtonText className="text-typography-0">Save</ButtonText>
+          <ButtonText className="text-typography-0">Log in</ButtonText>
         </Button>
       </VStack>
+
+      <VStack space="md" className="mt-4">
+        <Text className="text-typography-500 text-center">
+          Or log in with Google
+        </Text>
+        <GoogleSigninButton
+          onPress={GoogleSignIn}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          disabled={googleLoading}
+        />
+      </VStack>
+
       {error && <Text className="text-red-500 mt-2">{error}</Text>}
     </FormControl>
   );
