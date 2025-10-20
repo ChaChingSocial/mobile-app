76a238ba235d9d0c63a950d0d7acc0f8a72f81ba
moza88 <ozamabel@gmail.com>
2025-10-20 10:29:12 -0500

    made minor changes and removed extra navbar on singleCommunities

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -67,7 +67,7 @@ export default function ProtectedLayout() {
           name="communities"
           options={{
             title: "Community",
-            headerShown: true,
+            headerShown: false,
           }}
         />
         <Stack.Screen
